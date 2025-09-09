ff63a57ac6b52fb7a677cc7077000196543ca073
ANANAzZzZz <vladscooltrol@gmail.com>
2025-09-09 10:44:03 +0300

    initial commit

---
+++ src/main/java/suai/vladislav/efstest/EfSTestApplication.java
@@ -0,0 +1,13 @@
+package suai.vladislav.efstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfSTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EfSTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/suai/vladislav/efstest/EfSTestApplicationTests.java
@@ -0,0 +1,13 @@
+package suai.vladislav.efstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfSTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
